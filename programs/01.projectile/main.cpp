31c0b1ffc573759c7ab5bae9d5693a33e8cb53a1
Tri M. Cao <trimcao@gmail.com>
2020-08-19 21:16:55 -0700

    reorganize the code to allow writing mini-programs that make use of the raytracer library

---
+++ programs/01.projectile/main.cpp
@@ -0,0 +1,50 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+// #define DOCTEST_CONFIG_DISABLE
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../../include/Tuple.h"
+#include "../../include/Point.h"
+#include "../../include/Vector.h"
+#include "../../include/Util.h"
+
+struct Projectile
+{
+    Point Position;
+    Vector Velocity;
+};
+
+struct Environment
+{
+    Vector Gravity;
+    Vector Wind;
+};
+
+Projectile Tick(const Environment &Env, const Projectile &Proj);
+
+int main(int argc, char **argv)
+{
+    Projectile P;
+    P.Position = Point(0.f, 1.f, 0.f);
+    P.Velocity = Vector(1.f, 1.f, 0.f).Normalize();
+
+    Environment E;
+    E.Gravity = Vector(0.f, -0.1f, 0.f);
+    E.Wind = Vector(-0.01f, 0.f, 0.f);
+
+    while (P.Position.Y >= 0)
+    {
+        std::cout << "Current Position: " << P.Position << "\n";
+        P = Tick(E, P);
+    }
+
+    return 0;
+}
+
+Projectile Tick(const Environment &Env, const Projectile &Proj)
+{
+    Projectile NewProj;
+    NewProj.Position = Proj.Position + Proj.Velocity;
+    NewProj.Velocity = Proj.Velocity + Env.Gravity + Env.Wind;
+    return NewProj;
+}
