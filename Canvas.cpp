edeef9b8dfeff9345e60b05a634a28e2fe023ec2
Tri M. Cao <trimcao@gmail.com>
2020-08-21 18:25:00 -0700

    complete the basic functions of Canvas class

--- Canvas.cpp
+++ Canvas.cpp
@@ -1,4 +1,8 @@
 #include "include/Canvas.h"
+#include <sstream>
+
+const int Canvas::MAX_LINE_LENGTH = 70;
+const int Canvas::MAX_COLOR_VALUE = 255;
 
 Canvas::Canvas() {}
 
@@ -9,6 +13,15 @@ Canvas::Canvas(int Width, int Height)
     // set default max color value to 255
     maxColorValue = 255;
     canvas = std::vector<std::vector<Color>>(Height, std::vector<Color>(Width, Color(0.f, 0.f, 0.f)));
+}
+
+Canvas::Canvas(int Width, int Height, Color C)
+{
+    width = Width;
+    height = Height;
+    // set default max color value to 255
+    maxColorValue = 255;
+    canvas = std::vector<std::vector<Color>>(Height, std::vector<Color>(Width, C));
 }
 
 int Canvas::GetWidth()
@@ -51,6 +64,35 @@ std::string Canvas::ToPPM()
     s += std::to_string(height) + '\n';
     s += "255\n";
 
+    for (auto Row : canvas)
+    {   
+        int LineLength = 0;
+        for (auto C : Row)
+        {
+            auto PPMVals = C.ToPPMVal(MAX_COLOR_VALUE);
+            for (auto Val : PPMVals)
+            {
+                std::string ValStr = std::to_string(Val);
+                if (LineLength == 0) {}
+                else if (LineLength + ValStr.length() + 1 > MAX_LINE_LENGTH)
+                {
+                    s += '\n';
+                    LineLength = 0;
+                }
+                else
+                {
+                    s += ' ';
+                    LineLength += 1;
+                }
+                    
+                s += ValStr;
+                LineLength += ValStr.length();
+            }
+        }
+
+        s += '\n';
+    }
+
     return s;
 }
 
@@ -68,4 +110,51 @@ TEST_CASE("writing pixels to a canvas")
     Color Red = Color(1.f, 0.f, 0.f);
     A.WritePixel(2, 3, Red);
     CHECK(*A.GetPixel(2, 3) == Color(1.f, 0.f, 0.f));
+}
+
+TEST_CASE("constructing the PPM pixel data")
+{
+    Canvas A = Canvas(5, 3);
+    Color C1 = Color(1.5f, 0.f, 0.f);
+    Color C2 = Color(0.f, 0.5f, 0.f);
+    Color C3 = Color(-0.5f, 0.f, 1.f);
+
+    A.WritePixel(0, 0, C1);
+    A.WritePixel(2, 1, C2);
+    A.WritePixel(4, 2, C3);
+
+    const char *Expected = R"(P3
+5 3
+255
+255 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 128 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 255
+)";
+
+    std::string Generated = A.ToPPM();
+    CHECK(Generated == std::string(Expected));
+
+    // check if last character in the file is a newline character
+    CHECK(Generated[Generated.length()-1] == '\n');
+
+    // split string by lines
+    // auto result = std::vector<std::string>{};
+    // std::stringstream ss(A.ToPPM());
+
+    // for (std::string line; std::getline(ss, line, '\n');)
+    //     result.push_back(line);
+}
+
+TEST_CASE("splitting long lines in PPM files")
+{
+    Canvas A = Canvas(10, 2, Color(1.f, 0.8f, 0.6f));
+    const char *Expected = R"(P3
+10 2
+255
+255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204
+153 255 204 153 255 204 153 255 204 153 255 204 153
+255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204
+153 255 204 153 255 204 153 255 204 153 255 204 153
+)";
+    CHECK(A.ToPPM() == std::string(Expected));
 }
--- include/Canvas.h
+++ include/Canvas.h
@@ -11,11 +11,14 @@ class Canvas
 private:
     int width, height;
     int maxColorValue;
+    static const int MAX_LINE_LENGTH;
+    static const int MAX_COLOR_VALUE;
     std::vector<std::vector<Color>> canvas;
 
 public:
     Canvas();
     Canvas(int Width, int Height);
+    Canvas(int Width, int Height, Color C);
 
     int WritePixel(int X, int Y, Color C);
     Color *GetPixel(int X, int Y);
