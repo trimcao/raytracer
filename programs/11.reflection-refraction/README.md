75d655eff428e59a2ae1ebf28bffdf81ec440104
Tri M. Cao <trimcao@gmail.com>
2020-10-03 18:19:02 -0700

    add another scene to showcase refraction

--- programs/11.reflection-refraction/README.md
+++ programs/11.reflection-refraction/README.md
@@ -9,3 +9,7 @@ Refraction in action. We can see how the glass sphere bends the light.
 ![alt text](refraction.png "Refraction first example")
 
 Why the bottom part of the glass sphere is black? That's because the glass sphere inverts the scene, the black part is actually the black "sky" above. In other words, when we look at the glass sphere, we see an upside-down mini version of the scene.
+
+Here's a better scene to showcase refraction with two opaque spheres behind one large transparent sphere (we can call it a crystal ball).
+
+![alt text](refraction2.png "Refraction first example")
--- programs/11.reflection-refraction/main.cpp
+++ programs/11.reflection-refraction/main.cpp
@@ -31,12 +31,12 @@ int main(int argc, char **argv)
     Mat.SetPattern(std::make_shared<CheckersPattern>(Color(0.75, 0.25, 0.), Color(1., 0.9, 0.9)));
     Floor.SetMaterial(Mat);
 
-    // Sphere LeftWall(1);
-    // LeftWall.SetTransform(Matrix::Scaling(10., 0.01, 10.)
+    // Sphere FrontWall(1);
+    // FrontWall.SetTransform(Matrix::Scaling(10., 0.01, 10.)
     //                             .RotateX(M_PI / 2)
     //                             .RotateY(-M_PI / 4)
     //                             .Translate(0., 0., 5.));
-    // LeftWall.SetMaterial(Floor.GetMaterial());
+    // FrontWall.SetMaterial(Floor.GetMaterial());
 
     // Sphere RightWall(2);
     // RightWall.SetTransform(Matrix::Scaling(10., 0.01, 10.)
@@ -46,28 +46,28 @@ int main(int argc, char **argv)
     // RightWall.SetMaterial(Floor.GetMaterial());
 
     Sphere Middle(3);
-    Middle.SetTransform(Matrix::Translation(-0.5, 1., 0.5));
+    Middle.SetTransform(Matrix::Scaling(0.5, 0.5, 0.5).Translate(-0.5, 0.5, 4.));
     Mat = Material();
     Mat.SetColor(Color(0.1, 1., 0.5));
     Mat.SetDiffuse(0.7);
     Mat.SetSpecular(0.3);
-    std::shared_ptr<Pattern> Ring = std::make_shared<StripePattern>(Color(0., 0., 0.), Color(0.98, 0.035, 0.043));
+    std::shared_ptr<Pattern> Ring = std::make_shared<StripePattern>(Color(0.5, 1., 0.1), Color(0.98, 0.035, 0.043));
     Ring->SetTransform(Transformations::Scaling(0.2, 1., 1.));
     Mat.SetPattern(Ring);
     Middle.SetMaterial(Mat);
 
     Sphere Right(4);
-    Right.SetTransform(Matrix::Scaling(0.5, 0.5, 0.5).Translate(1.5, 0.5, -0.5));
+    // Right.SetTransform(Matrix::Scaling(0.5, 0.5, 0.5).Translate(0.7, 0.5, 1.));
+    Right.SetTransform(Matrix::Scaling(0.33, 0.33, 0.33).Translate(0.2, 0.33, 3.));
     Mat = Material();
-    Mat.SetColor(Color(0.5, 1., 0.1));
+    Mat.SetColor(Color(0.5, 0.1, 1.));
     Mat.SetDiffuse(0.7);
     Mat.SetSpecular(0.3);
     Right.SetMaterial(Mat);
 
-    auto Left = Sphere::GlassSphere();
-    // Sphere Left(5);
-    Left.SetTransform(Matrix::Scaling(0.33, 0.33, 0.33).Translate(-1.5, 0.33, -0.75));
-    Mat = Left.GetMaterial();
+    auto Front = Sphere::GlassSphere();
+    Front.SetTransform(Matrix::Scaling(1., 1., 1.).Translate(0., 1., -1.5));
+    Mat = Front.GetMaterial();
     Mat.SetColor(Color(0.8, 0.8, 0.9));
     Mat.SetAmbient(0.);
     Mat.SetDiffuse(0.2);
@@ -75,19 +75,19 @@ int main(int argc, char **argv)
     Mat.SetShininess(300.);
     Mat.SetTransparency(0.8);
     Mat.SetRefractiveIndex(1.57);
-    Left.SetMaterial(Mat);
+    Front.SetMaterial(Mat);
 
     World W;
     Light L(Color(1., 1., 1.), Point(-10., 10., -10.));
     W.SetLight(L);
     W.AddObject(Floor);
-    // W.AddObject(LeftWall);
+    // W.AddObject(FrontWall);
     // W.AddObject(RightWall);
     W.AddObject(Middle);
     W.AddObject(Right);
-    W.AddObject(Left);
+    W.AddObject(Front);
 
-    Camera Cam(800, 400, M_PI/3);
+    Camera Cam(640, 480, M_PI/3);
     Cam.SetTransform(Transformations::ViewTransform(Point(0.1, 1.5, -5.),
                                                     Point(0., 1., 0.),
                                                     Vector(0., 1., 0.)));
@@ -100,7 +100,7 @@ int main(int argc, char **argv)
     bool RenderShadow = true;
     auto CV = Cam.Render(W, RenderShadow, true, 5);
 
-    std::ofstream out("refraction.ppm");
+    std::ofstream out("refraction2.ppm");
     out << CV.ToPPM();
     out.close();
 
