cc6931d4a424f419303fa1c5719b64846b49051b
Tri M. Cao <trimcao@gmail.com>
2020-12-20 12:42:52 -0800

    reorginize the project structure

---
+++ cmd/README.md
@@ -0,0 +1,1 @@
+## Command-line interface of the ray tracer
---
+++ cmd/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <ryml.hpp>
+
+#include "Point.h"
+#include "Vector.h"
+#include "Color.h"
+#include "Canvas.h"
+#include "Sphere.h"
+#include "Ray.h"
+#include "Light.h"
+#include "Intersection.h"
+#include "World.h"
+#include "Camera.h"
+#include "Transformations.h"
+#include "Plane.h"
+#include "Pattern.h"
+#include "Cubes.h"
+#include "Cylinders.h"
+#include "Triangles.h"
+#include "ObjParser.h"
+
+int main(int argc, char **argv)
+{
+    std::cout << "Hello from cmd\n";
+
+    return 0;
+}
